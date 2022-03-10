8473de493a30197bd70559e22f1eac2853b303ed
Chris Garrard <cgarrard@union.k12.ms.us>
2022-03-10 10:03:47 -0600

    Initial Commit

---
+++ ontheroadagain/ontheroadagainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ontheroadagainApp.swift
+//  ontheroadagain
+//
+//  Created by Chris Garrard on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ontheroadagainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
