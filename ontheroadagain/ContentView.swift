88215c64879be39e41571df11814e0769bde1fbd
Jennifer Easterling <jeasterling@union.k12.ms.us>
2022-03-11 09:51:52 -0600

    Change text label - does this work?

--- ontheroadagain/ContentView.swift
+++ ontheroadagain/ContentView.swift
@@ -26,7 +26,7 @@ struct ContentView: View {
             List() {
             //add our aditonal views here
             NavigationLink(destination: Startinglocation())
-            {Text("Get Started")}
+            {Text("Does this work?")}
             NavigationLink(destination: Login())
             {Text("Login")}
                 
